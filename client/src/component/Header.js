188f430784dd37c6a4eaff3a013e55c61d054d7e
Umpoy <iumpoy@yahoo.com>
2019-03-04 10:58:13 -0800

    added css background

---
+++ client/src/component/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="header">
+            <h1>Newsletter App</h1>
+        </div>
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './component/App';
+import './assets/css/style.css'
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
