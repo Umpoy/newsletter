fb26383f4491eb0d5889a23b7bee9fb0d1ab376d
Umpoy <iumpoy@yahoo.com>
2019-03-04 10:24:16 -0800

    hiding configurations

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const nodemailer = require('nodemailer');
+const config = require('./config.js')
+
+app.get('/send', (req, res) => {
+    const trasporter = nodemailer.createTransport({
+        service: 'gmail',
+
+    })
+})
 
 app.listen(port, () => {
     console.log(`Sever running on locoalhost:${port}`);
