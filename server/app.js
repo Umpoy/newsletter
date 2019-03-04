8c492d946f6aa93b78133a4f32a179c45d6a985d
Umpoy <iumpoy@yahoo.com>
2019-03-04 10:29:56 -0800

    nodemailer up and running

--- server/app.js
+++ server/app.js
@@ -5,9 +5,27 @@ const nodemailer = require('nodemailer');
 const config = require('./config.js')
 
 app.get('/send', (req, res) => {
-    const trasporter = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
         service: 'gmail',
+        auth: {
+            user: config.email,
+            pass: config.pass
+        }
+    });
 
+    const mailOptions = {
+        from: config.email,
+        to: config.email, // change this to users email in the future
+        subject: "GEM Newsletters",
+        html: "<h1>Hello World</h1><p>We sent this email using nodemailer 🤓</p>"
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email send: ' + info.response);
+        }
     })
 })
 
