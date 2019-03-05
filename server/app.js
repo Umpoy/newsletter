15f8d16743e05e9950c69bf3d63618710c295619
Umpoy <iumpoy@yahoo.com>
2019-03-04 19:32:37 -0800

    added server config

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const port = 5000;
+// const cors = require('cors');
+const path = require('path');
+const port = 5003;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 require('./app/routes.js')(app);
 
