b1a97338c87c5bf8e2136d6be7457e4f412dcd68
Umpoy <iumpoy@yahoo.com>
2019-03-05 09:30:15 -0800

    adding build folder

---
+++ client/build/precache-manifest.c2c8cdc310bf66f7744e0aa5462ac16b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d55a8e0895d37bfce19d",
+    "url": "/static/js/main.d55a8e08.chunk.js"
+  },
+  {
+    "revision": "0accef2a07fc0896f9a6",
+    "url": "/static/js/2.0accef2a.chunk.js"
+  },
+  {
+    "revision": "d55a8e0895d37bfce19d",
+    "url": "/static/css/main.9fce7140.chunk.css"
+  },
+  {
+    "revision": "b9a3963808505abf208eb56d3cf2a68d",
+    "url": "/index.html"
+  }
+];
